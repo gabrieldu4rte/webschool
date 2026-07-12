a732477a41144edae2d244cab6e1877e7289b3a3
gabrieldu4rte <gabrielduartedt69@gmail.com>
2026-07-11 21:33:22 -0300

    feat: adição de novas interfaces para estruturação do projeto

---
+++ WebSchool.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSchool.Domain.Entities;
+
+namespace WebSchool.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(int id);
+        Task<List<User>> GetAllAsync();
+
+        Task<User> AddAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<User> DeleteAsync(int id);
+    }
+}
