233a2b88693a0d33908dd643ed0f18d41319f04d
gabrieldu4rte <gabrielduartedt69@gmail.com>
2026-07-13 22:32:37 -0300

    feat: criação dos DTOs de curso

---
+++ WebSchool.Application/DTOs/Course/CourseGetDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSchool.Domain.Entities;
+
+namespace WebSchool.Application.DTOs.Course
+{
+    public class CourseGetDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
